1896b1b938d4c31821261f93812733096ad1bd32
Pons <suyambu.vps@gmail.com>
2020-05-09 15:20:40 +0200

    Create Nav graph

--- Covid19/app/src/main/java/in/ponshere/covid19app/MainActivity.kt
+++ Covid19/app/src/main/java/in/ponshere/covid19app/MainActivity.kt
@@ -2,11 +2,18 @@ package `in`.ponshere.covid19app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navigation_host_fragment) as NavHostFragment?
+//        NavigationUI.setupWithNavController(
+//            bottomNavigationView,
+//            navHostFragment!!.navController
+//        )
     }
 }
---
+++ Covid19/app/src/main/java/in/ponshere/covid19app/fragments/MapFragment.kt
@@ -0,0 +1,18 @@
+package `in`.ponshere.covid19app.fragments
+
+import `in`.ponshere.covid19app.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MapFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.total_stats_fragment, container, false)
+    }
+}
